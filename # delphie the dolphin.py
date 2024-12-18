71cd49006b0183d9499019ce98d589577abba8a3
zydre <yesjiyu@gmail.com>
2024-12-18 12:05:37 -0500

    Add files via upload
    unfinished

---
+++ # delphie the dolphin.py
@@ -0,0 +1,46 @@
+# delphie the dolphin
+
+print("hai!!! im a dolphin!")
+print("you can't see me")
+print("im inside your computer...")
+print("im not a virus")
+print("but i would like to interview you")
+
+#hi dolphin
+print("i almost forgot...")
+print ("my name is delphie")
+print("this is me :D")
+print("         ,-._")
+print("       _.-'  '--.")
+print("     .'      _  -`\_")
+print("    / .----.`_.'----' ")
+print("    ;/     `")
+print("   /_;")
+
+print("ill show you my magic trick")
+print("ok! i have my eyes closed you write down the answers to the questions you ask")
+
+color = str (input("alright,,, whats your favorite color?: "))
+print("done?")
+num = int(input("whats your favorite single digit number?: "))
+print("goodgood...")
+
+if num >= 5:
+    print("i guess... your favorite number is greater than or equal to five..")
+else: ("i guess,,, your favorite number is smaller than five")
+
+print("delphie is thinking....")
+
+print (str("is your favorite number... " + num + "?"))
+print("hehehe.. im right aren't i")
+
+def dolphin ():
+    print
+#the 
+
+
+
+
+
+
+
