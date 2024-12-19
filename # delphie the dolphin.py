f9e3c52ae8ef52724760cab92e1cdcadd49a683f
zydre <yesjiyu@gmail.com>
2024-12-19 11:53:53 -0500

    delphie update
    termcolor not working

--- # delphie the dolphin.py
+++ # delphie the dolphin.py
@@ -1,4 +1,5 @@
 # delphie the dolphin
+pip3 install termcolor
 
 print("hai!!! im a dolphin!")
 print("you can't see me")
@@ -19,24 +20,53 @@ print("   /_;")
 
 print("ill show you my magic trick")
 print("ok! i have my eyes closed you write down the answers to the questions you ask")
+print ("*delphie's eyes are closed, she cannot see your input*")
 
-color = str (input("alright,,, whats your favorite color?: "))
-print("done?")
+#number
 num = int(input("whats your favorite single digit number?: "))
-print("goodgood...")
+print("good good...")
 
-if num >= 5:
-    print("i guess... your favorite number is greater than or equal to five..")
-else: ("i guess,,, your favorite number is smaller than five")
+# check if the number is greater than, less than, or equal to 5
+if num > 5:
+    print("i guess,,, your favorite number is greater than five")
+elif num < 5:
+    print("i guess,,, your favorite number is smaller than five")
+elif num == 5:
+    print("hmmm.....")
 
 print("delphie is thinking....")
+#concatanate
+print("is your favorite number... " + str(num) + "?")
+print("hehehe.. i'm right, aren't i?")
 
-print (str("is your favorite number... " + num + "?"))
-print("hehehe.. im right aren't i")
+# color
+from termcolor import colored
 
-def dolphin ():
-    print
-#the 
+color= input("What's your favorite color? ").lower()
+
+#options
+colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+
+if color in colors:
+    print(colored("Let me guess... Your favorite color is " + color + "!", color))
+else:
+    print("ooops,,, delphie can't print that color.. try one of these: red, green, yellow, blue, magenta, cyan, or white.")
+
+answer = str(input("did I get it right? (yes/no): "))
+
+if answer == "yes":
+    print("yayy!!!@11!!!1")
+else: 
+    print ("oops...sorry :(")
+
+
+
+
+
+
+
+
+
 
 
 
